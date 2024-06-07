fc1ff18c9e2d7349de9e2bb811673fc63dd3b41d
Viet Pham <105752177+LePoisson104@users.noreply.github.com>
2024-06-06 19:54:53 -0500

    Update location_link.js

--- Helper/location_link.js
+++ Helper/location_link.js
@@ -1,2 +1,2 @@
-export const front_end_url = "https://lepoisson104.github.io/";
+export const front_end_url = "https://lepoisson104.github.io";
 export const back_end_url = "https://zoo-server-dc4b.onrender.com";
